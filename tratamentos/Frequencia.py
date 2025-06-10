bd94f4673e65eedb834b48c45f7ff86a1c8a0e94
F3gcastro <gcastro@f3capital.com.br>
2025-06-09 21:38:44 -0300

    Ajuste nos logs do trat. de Frequência conforme o resto do código.

--- tratamentos/Frequencia.py
+++ tratamentos/Frequencia.py
@@ -1,3 +1,5 @@
+import os.path
+
 from interfaces.ITratamento import ITratamentoDados
 import pyspark.sql.functions as F
 from pyspark.sql.functions import format_number as format
@@ -15,34 +17,34 @@ class Frequencia(ITratamentoDados):
     
     
     def __tratar_dado__(self) -> None:
-        print(self.nome_sensor + " is working")
+        print("\t\t" + self.nome_sensor + " is working")
 
         nome_arquivo = self.utils.get_data_s3_csv(EnumBuckets.RAW.value)
-        print(nome_arquivo)
+        print("\t\t" + nome_arquivo)
 
         df = self.spark.read.option("multiline", "true").json(nome_arquivo)
         df.printSchema()
         df.show()
         
         df = self.utils.remove_null(df)
-        print("removendo nulls")
+        print("\t\tremovendo nulls")
         df.show()
 
         df = self.utils.filter_by_sensor(df, "valueType", "Hz")
 
         df = self.utils.remove_wrong_float(df, "value")
-        print("removendo campos que não sao float da coluna frquence")
+        print("\t\tremovendo campos que não sao float da coluna frquence")
         df.show()
 
         df = self.utils.format_number(df, "value")
-        print("formatando numeros")
+        print("\t\tformatando numeros")
         df.printSchema()
         df.show()
 
-        print("ordenando por data")
+        print("\t\tordenando por data")
         df = self.utils.order_by_coluna_desc(df, "instant")
 
-        print("gerando arquivo")
+        print("\t\tgerando arquivo")
         object_name = self.utils.transform_df_to_json(df, self.tipo_dado, "trusted")
 
 
