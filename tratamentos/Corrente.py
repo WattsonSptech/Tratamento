4636bd99d42c79364de59c4f23f65c0a03dbc3c4
Caetano Resende Domingues <caetano.domingues@sptech.school>
2025-06-17 00:47:06 -0300

    Revert "refactor: alteração na formação do trusted e client de corrente"
    This reverts commit e585089ea3a45fa2cfd744a42bb9388193a666cd.

--- tratamentos/Corrente.py
+++ tratamentos/Corrente.py
@@ -15,63 +15,17 @@ class Corrente(ITratamentoDados):
     def __tratar_dado__(self) -> None:
         nome_arquivo = self.utils.get_data_s3_csv(EnumBuckets.RAW.value)
 
-        df_corrente = self.spark.read.option("multiline", "true").json(nome_arquivo)
-        df_tensao = self.spark.read.option("multiline", "true").json(nome_arquivo)
-        df_potencia = self.spark.read.option("multiline", "true").json(nome_arquivo)
-        df_temperatura = self.spark.read.option("multiline", "true").json(nome_arquivo)
-
+        df = self.spark.read.option("multiline", "true").json(nome_arquivo)
+        df.printSchema()
         
-        df_corrente = self.utils.filter_by_sensor(df_corrente, "valueType", "amp\u00e9re")
-        df_tensao = self.utils.filter_by_sensor(df_tensao, "valueType", "volts")
-        df_potencia = self.utils.filter_by_sensor(df_potencia, "valueType", "fator")
-        df_temperatura = self.utils.filter_by_sensor(df_temperatura, "valueType", "\u00b0C")
-
-        df_tensao = self.utils.drop_column(df_tensao, "valueType")
-        df_tensao = self.utils.drop_column(df_tensao, "zone")
-        df_tensao = self.utils.drop_column(df_tensao, "scenery")
-        df_tensao = self.utils.drop_column(df_tensao, "instant")
-        df_tensao = self.utils.format_number_to_float(df_tensao, "value")
-        df_tensao = self.utils.remove_wrong_float(df_tensao, "value")
-        df_tensao = self.utils.order_by_coluna_desc(df_tensao, "value")
-        df_tensao = self.utils.rename_column(df_tensao, "value", "voltage")
-        df_tensao = self.utils.enumerate_column(df_tensao, "id")
-    
-        df_tensao.show()
-
-        df_potencia = self.utils.drop_column(df_potencia, "valueType")
-        df_potencia = self.utils.drop_column(df_potencia, "zone")
-        df_potencia = self.utils.drop_column(df_potencia, "scenery")
-        df_potencia = self.utils.drop_column(df_potencia, "instant")
-        df_potencia = self.utils.format_number_to_float(df_potencia, "value")
-        df_potencia = self.utils.remove_wrong_float(df_potencia, "value")
-        df_potencia = self.utils.rename_column(df_potencia, "value", "power_factor")
-        df_potencia = self.utils.enumerate_column(df_potencia, "id")
-
-        df_potencia.show()
-
-        df_temperatura = self.utils.drop_column(df_temperatura, "valueType")
-        df_temperatura = self.utils.drop_column(df_temperatura, "zone")
-        df_temperatura = self.utils.drop_column(df_temperatura, "scenery")
-        df_temperatura = self.utils.drop_column(df_temperatura, "instant")
-        df_temperatura = self.utils.format_number_to_float(df_temperatura, "value")
-        df_temperatura = self.utils.remove_wrong_float(df_temperatura, "value")
-        df_temperatura = self.utils.rename_column(df_temperatura, "value", "temperature")
-        df_temperatura = self.utils.enumerate_column(df_temperatura, "id")
-
-        df_temperatura.show()
-
-        df_corrente = self.utils.remove_null(df_corrente)
-        df_corrente = self.utils.remove_wrong_float(df_corrente, "value")
-        df_corrente = self.utils.rename_values(df_corrente, "valueType", "amp\u00e9re", "Ampere")
-        df_corrente = self.utils.enumerate_column(df_corrente, "id")
-        df_corrente = df_corrente.join(df_tensao, "id")
-        df_corrente = df_corrente.join(df_potencia, "id")
-        df_corrente = df_corrente.join(df_temperatura, "id")
+        df = self.utils.filter_by_sensor(df, "valueType", "amp\u00e9re")
+        df = self.utils.remove_null(df)
+        df = self.utils.rename_values(df, "valueType", "amp\u00e9re", "Ampere")
+        df = self.utils.order_by_coluna_desc(df, "value")
+        df.printSchema()
+        df.show()
         
-        df_corrente.printSchema()
-        df_corrente.show()
-        
-        object_name = self.utils.transform_df_to_json(df_corrente, self.tipo_dado, "trusted")
+        object_name = self.utils.transform_df_to_json(df, self.tipo_dado, "trusted")
         self.utils.set_data_s3_file(object_name, EnumBuckets.TRUSTED.value)
 
         self.__gerar_arquivo_client__()
@@ -79,17 +33,15 @@ class Corrente(ITratamentoDados):
     def __gerar_arquivo_client__(self) -> None:
         arquivo_corrente = self.utils.get_data_s3_csv(bucket_name=EnumBuckets.TRUSTED.value, sensor=self.tipo_dado)
         df = self.spark.read.option("multiline", "true").json(arquivo_corrente)
-        df = self.utils.rename_column(df, "value", "secundary_current")
-        df = self.utils.format_number_to_float(df, "secundary_current")
-        df = self.utils.format_number_to_float(df, "voltage")
-        df = self.utils.format_number_to_float(df, "power_factor")
-        df = self.utils.format_number_to_float(df, "temperature")
+        df.printSchema()
         df = self.utils.drop_column(df, "zone")
         df = self.utils.drop_column(df, "valueType")
+        df = self.utils.format_number_to_float(df, "value")
+        df = self.utils.remove_wrong_float(df, "value")
         df = self.utils.order_by_coluna_desc(df, "value")
+        df = self.utils.rename_column(df, "value", "secundary_current")
         df = self.__encontrar_corrente_primaria__(df)
-        df = self.__encontrar_corrente_curto_circuito__(df)
-        df = self.utils.order_by_coluna_asc(df,"id")
+        df = self.utils.enumerate_column(df, "id")
         df.printSchema()
         df.show()
 
@@ -98,8 +50,5 @@ class Corrente(ITratamentoDados):
 
     def __encontrar_corrente_primaria__(self,df):
         return df.withColumn("primary_current",(F.col("secundary_current") * 160).cast("int"))
-    
-    def __encontrar_corrente_curto_circuito__(self,df):
-        return df.withColumn("short_circuit_current",(F.col("voltage") / 40).cast("int"))
 
     
