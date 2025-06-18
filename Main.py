b73a3a9e281a087aba695c91a52e424a38cf0ca4
F3gcastro <gcastro@f3capital.com.br>
2025-06-17 21:57:44 -0300

    Melhorando o processo geral de execução.

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import os.path
 import dotenv
 from glob import glob
+from tqdm import tqdm
 from tratamentos.Frequencia import Frequencia
 from tratamentos.Harmonica import Harmonica
 from tratamentos.Tensao import Tensao
@@ -8,22 +9,31 @@ from tratamentos.Temperatura import Temperatura
 import time
 
 
-def chamar_funcoes():
+def chamar_funcoes(dev_mode):
     sensores = (Frequencia, Harmonica, Tensao, Temperatura)
     for sensor in sensores:
         try:
             print(f"\n\tIniciando tratamento de {sensor.__name__}...\n")
             sensor().__tratar_dado__()
-        except Exception:
+        except Exception as e:
             print(f"\t\033[31m[!] Erro no sensor de {sensor.__name__}!\033[0m")
+            if dev_mode:
+                raise e
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
+    dev_mode = os.getenv("DEV_MODE", 0) == "1"
+    treatment_timeout = int(os.getenv("TREATMENT_TIMEOUT", "50"))
 
     if not os.path.exists("./temp"):
         os.mkdir("./temp")
 
     while True:
-        chamar_funcoes()
         [os.remove(f) for f in glob("./temp/*")]
-        time.sleep(60*5)
+
+        chamar_funcoes(dev_mode)
+        print("Tratamentos concluídos!")
+
+        print("\nTempo para a próxima execução...")
+        for i in tqdm(range(treatment_timeout)):
+            time.sleep(1)
