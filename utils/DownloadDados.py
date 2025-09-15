787ca89dd0a6a1fb59643a603ae254e05e312044
edu-correa <eduardo.correa@sptech.school>
2025-09-14 21:34:58 -0300

    feat: subindo o resto

---
+++ utils/DownloadDados.py
@@ -0,0 +1,34 @@
+import requests
+from google.cloud import bigquery
+import basedosdados as bd
+import json
+import datetime
+
+class DownloadDados:
+    def __init__(self) -> None:
+        print("Contato com download")
+
+    def consultarPorUrl(self, url):
+        response = requests.get(url).text
+        print(response)
+        data = json.loads(response)
+        print(data)
+        return data
+    
+    def consultarPorQueryBase(self, query):
+        query = """
+        SELECT
+            dados.ano as ano,
+            dados.mes as mes,
+            dados.sigla_uf AS sigla_uf,
+            diretorio_sigla_uf.nome AS sigla_uf_nome,
+            dados.tipo_consumo as tipo_consumo,
+            dados.numero_consumidores as numero_consumidores,
+            dados.consumo as consumo
+        FROM `basedosdados.br_mme_consumo_energia_eletrica.uf` AS dados
+        LEFT JOIN (SELECT DISTINCT sigla,nome  FROM `basedosdados.br_bd_diretorios_brasil.uf`) AS diretorio_sigla_uf
+            ON dados.sigla_uf = diretorio_sigla_uf.sigla
+        """
+
+        return bd.read_sql(query = query, billing_project_id='projeto-consulta-wattson').text
+
